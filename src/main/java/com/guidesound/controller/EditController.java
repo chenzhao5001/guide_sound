5f241842a83b33addb6644b8e56a444092256665
chenzhao <chenzhao5001@163.com>
2018-10-14 01:40:12 +0800

    图片上传逻辑

--- src/main/java/com/guidesound/controller/EditController.java
+++ src/main/java/com/guidesound/controller/EditController.java
@@ -1,11 +1,11 @@
 package com.guidesound.controller;
 
 import com.guidesound.util.ServiceResponse;
-import org.apache.commons.io.IOUtils;
+import okhttp3.*;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -15,18 +15,20 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequestMapping("/edit")
 public class EditController {
 
+    public static final String TAG = "UploadHelper";
+    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
+    private final OkHttpClient client = new OkHttpClient();
+
     @RequestMapping(value = "/upload")
     @ResponseBody
     public Object upload(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-
-        System.out.println(1234);
         MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
         MultipartFile picture = multipartRequest.getFile("upload");
         System.out.println(picture);
@@ -37,33 +39,48 @@ public class EditController {
             serviceResponse.data = null;
             return serviceResponse;
         }
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss_");//设置日期格式
+        String strDate = df.format(new Date());// new Date()为获取当前系统时间
         String savePath = multipartRequest.getServletContext().getRealPath("");
-        System.out.println(savePath);
         File file = new File(savePath);
         savePath = file.getParent() + "/edit_upload";
-
         File filePath = new File(savePath);
         if (!filePath.exists() && !filePath.isDirectory()) {
             filePath.mkdir();
         }
-        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss_");//设置日期格式
-        String strDate = df.format(new Date());// new Date()为获取当前系统时间
         String pathPic = savePath + "/" + strDate + picture.getOriginalFilename();
+        File temp = new File(pathPic);
+        picture.transferTo(temp);
+        RequestBody fileBody = RequestBody.create(MediaType.parse("image/png"), temp);
 
-        picture.transferTo(new File(pathPic));
+        OkHttpClient okHttpClient  = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
 
-        String showPicPath = "http://" + request.getServerName()
-                + ":"+ request.getServerPort()
-                + "/edit_upload"
-                + "/" + strDate
-                + picture.getOriginalFilename();
+        MultipartBody formBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("upload",strDate + picture.getOriginalFilename(),fileBody)
+                .addFormDataPart("sign","guide_sound")
+                .build();
 
-//        return null;
-        System.out.println(showPicPath);
+        Request req = new Request.Builder()
+                .url("http://139.199.123.168/fileservice/upload")
+                .post(formBody)
+                .build();
+        Response resp;
+        resp = client.newCall(req).execute();
+        String jsonString = resp.body().string();
+        System.out.println(jsonString);
+        JSONObject json = new JSONObject(jsonString);
+        String url = json.getString("data");
+
         EditResp editResp = new EditResp();
         editResp.setFileName(picture.getOriginalFilename());
         editResp.setUploaded(1);
-        editResp.setUrl("/edit_upload/" + strDate + picture.getOriginalFilename());
+        editResp.setUrl(url);
         return editResp;
     }
     @RequestMapping(value = "/browse")
